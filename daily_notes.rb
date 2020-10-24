19f4d4df0a40731390c04a139df000426ca5bf76
Moe Jaber <mjaber18@gmail.com>
2020-10-24 13:20:05 -0700

    fixed daily_notes.rb

---
+++ daily_notes.rb
@@ -0,0 +1,21 @@
+10/24/2020
+I've formally switched from the Javascript track in Launch School to
+Ruby.
+The prospect of learning 2 languages, and more importantly learning JIT (just in time)
+learning, was too appealing to pass up.
+
+I'd love to get a job in software sooner rather than later, but rushing will not
+do me any good.  I want to remain focused and continue to push forward.
+
+I made it all the way through the prep material about 18 months ago before my son
+was born.  When it came time to commit to the core curriculum and enter my credit
+card information I paused.  I asked myself 'are you really ready to commit to this'.
+The price made sense but would I only be wasting my time and the instructors' time.
+
+I noticed I had skipped over the "Learning how to learn" and computer science videos
+the first time around.  As I reflected I noticed I still had a long way to go before
+I was truly prepared to tackle the core curriculum.
+
+Now, many months later I was able to tackle the prep material with a renewed vigor.
+I had gone through all of the texts before but this time things stuck a lot better.
+I feel focused, energized, and motivated!
