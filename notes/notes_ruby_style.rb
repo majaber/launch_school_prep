dde601da49230286f859e9e4b2e8206ba5cfaee7
Moe Jaber <mjaber18@gmail.com>
2020-10-24 13:34:54 -0700

    notes

---
+++ notes/notes_ruby_style.rb
@@ -0,0 +1,9 @@
+snake_case for : methods, variables, and files. ( All lower case letters and using
+underscores to separate words)
+
+CONSTANTS should be in all uppercase letters
+
+Curly braces {} for when working with do...end blocks on single line.
+
+Use PascalCase / CamelCase when declaring a class name.  Capitalize the first
+letter of each word. (no spaces)
