9d7e8068037b76525daab28ddfa0bf239c00f862
Moe Jaber <mjaber18@gmail.com>
2020-10-25 09:12:41 -0700

    10/25/20 upload

--- notes/notes_ruby_style.rb
+++ notes/notes_ruby_style.rb
@@ -1,3 +1,5 @@
+codecademy ruby track - notes -
+
 snake_case for : methods, variables, and files. ( All lower case letters and using
 underscores to separate words)
 
@@ -7,3 +9,13 @@ Curly braces {} for when working with do...end blocks on single line.
 
 Use PascalCase / CamelCase when declaring a class name.  Capitalize the first
 letter of each word. (no spaces)
+
+Three data types: Numeric, Boolean, & Strings
+
+Methods are special traits you can call on strings such as .length / .upcase / .downcase
+
+* 'gets' allows for user input when working with Ruby.
+If you add the method '.chomp' it will remove the extra line that would normally print after
+running 'gets'. It is not necessary but will help you avoid having extra lines everywhere.
+
+String interpolation = #{(variable)}
