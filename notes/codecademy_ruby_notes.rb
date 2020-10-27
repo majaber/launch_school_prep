51a5c64228fc76e65657f92a80eecdc058212b94
Moe Jaber <mjaber18@gmail.com>
2020-10-26 21:09:05 -0700

    hash notes

--- notes/codecademy_ruby_notes.rb
+++ notes/codecademy_ruby_notes.rb
@@ -26,3 +26,27 @@ ex:
 for variable in range # .. or ...
   puts variable
 end
+
+In Ruby - curly braces {} are == do (opens a block) and end (closes the block) statements
+
+Had some trouble with the following bit of code:
+
+xyz = 0
+loop do
+  print "Ruby!"
+  xyz += 1
+  break if xyz == 30
+end
+
+* The goal of the code was to print the string "Ruby!" 30 times.  By setting up a variable
+that was essentially a counter next to it, xyz.  We could run the loop and break once the counter
+incrememnted up to 30.  Since the counter variable (xyz) was never being printed, it did not
+impact our output whatsoever.
+
+* When using the .times iterator remember to place your action in curly braces
+
+10/26/20
+Worked on arrays and hashes.
+Hash.new or hash = {
+  "key" => "value"
+}
