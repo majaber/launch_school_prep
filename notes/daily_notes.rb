c123319bb79f7ce28f8d08cbb4e1592bb686ff97
Moe Jaber <mjaber18@gmail.com>
2020-11-02 22:25:15 -0800

    practice

---
+++ notes/daily_notes.rb
@@ -0,0 +1,9 @@
+11/01/20
+
+First notes on intro to programming with Ruby.
+One directory for each chapter.
+Create a separate file for every exercise.
+
+11/02/20
+Constants are denoted with all UPPERCASE letters
+    -a value that never changes
