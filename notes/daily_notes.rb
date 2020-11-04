85ba888ef191f8897323ceeb3a46d27ff6c3d737
Moe Jaber <mjaber18@gmail.com>
2020-11-03 21:39:09 -0800

    notes

--- notes/daily_notes.rb
+++ notes/daily_notes.rb
@@ -7,3 +7,20 @@ Create a separate file for every exercise.
 11/02/20
 Constants are denoted with all UPPERCASE letters
     -a value that never changes
+Rubygems
+- gem install <gem name>
+
+11/03/20
+Finally begin the intro to ruby textbook.  I spent the better part of yesterday
+working on my preparations and doing a few examples.
+
+I'm still trying to update my version of ruby.  I would much rather struggle with it now rather than later.
+
+- Spent my morning hour attempting to reinstall ruby and gems to the highest version.
+I believe my issue was rvm... It appears that I was able to uninstall ruby and reinstall with rvm
+I am now using v 2.7.2 and was able to successifully install the pry gem .
+
+PRY -
+where 'binding.pry' was declared- the program will open up a new pry session that allows you to debug and
+inspect the code.
+A better option that simply using puts as this allows you to see what's really happening with your code.
